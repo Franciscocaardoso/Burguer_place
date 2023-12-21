2039ad23e0244ac5a43689a495568956d9a868c2
IgorIA <igorsemphoski@gmail.com>
2023-12-20 22:54:52 -0300

    Implement unit tests for ProductController

--- api/src/main/java/br/com/senior/burger_place/exception/ExceptionHandlerError.java
+++ api/src/main/java/br/com/senior/burger_place/exception/ExceptionHandlerError.java
@@ -9,39 +9,49 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class ExceptionHandlerError {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity entityNotFoundException(EntityNotFoundException e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseError(e));
-    }
-
-    @ExceptionHandler(DuplicateKeyException.class)
-    public ResponseEntity duplicateKeyException(DuplicateKeyException e){
+    @ExceptionHandler({
+            EntityNotFoundException.class,
+            DuplicateKeyException.class
+    })
+    public ResponseEntity handleNotFound(EntityNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseError(e));
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
-        var error = e.getFieldErrors();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error.stream().map(DadosErroValidacao::new).toList());
+    @ExceptionHandler({
+            NoSuchElementException.class,
+            IllegalArgumentException.class,
+            MethodArgumentNotValidException.class
+    })
+    public ResponseEntity handleBadRequests(Exception exception) {
+        if (exception instanceof MethodArgumentNotValidException) {
+            List<FieldError> fieldErrors = ((MethodArgumentNotValidException) exception).getFieldErrors();
+
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body((fieldErrors.stream()
+                            .map(DadosErroValidacao::new)
+                            .sorted((Comparator.comparing(o -> o.field)))
+                            .toList()));
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseError(exception));
     }
 
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity handleNotFoundException (NoSuchElementException e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseError(e));
-    }
-
-    private record DadosErroValidacao(String field, String message){
-        public DadosErroValidacao(FieldError error){
+    private record DadosErroValidacao(String field, String message) {
+        public DadosErroValidacao(FieldError error) {
             this(error.getField(), error.getDefaultMessage());
         }
     }
-    private record ResponseError(String message){
-        public ResponseError(RuntimeException exception){
+
+    private record ResponseError(String message) {
+        public ResponseError(Exception exception) {
             this(exception.getMessage());
         }
     }
