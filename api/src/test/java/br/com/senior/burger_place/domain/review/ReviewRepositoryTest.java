af91ed22792ed4c656043ef69385311a52a69600
Franciscocaardoso <franciscoalyson.c@gmail.com>
2023-12-22 17:15:29 -0300

    Adicionando teste unitátio para classe ReviewRepository

--- api/src/test/java/br/com/senior/burger_place/domain/review/ReviewRepositoryTest.java
+++ api/src/test/java/br/com/senior/burger_place/domain/review/ReviewRepositoryTest.java
@@ -1,4 +1,15 @@
+package br.com.senior.burger_place.domain.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
 class ReviewRepositoryTest {
-  
+
+    @Test
+    public void verifyOccupationExists(){
+
+    }
 }
