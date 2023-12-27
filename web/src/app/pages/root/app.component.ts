29ac2116f91979355bcb1c012b493ae090de31bb
IgorIA <igorsemphoski@gmail.com>
2023-12-27 19:18:42 -0300

    create available boards page

--- web/src/app/pages/root/app.component.ts
+++ web/src/app/pages/root/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from '../header/header.component';
-import { SvgImageComponent } from '../svg-image/svg-image.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SvgImageComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
