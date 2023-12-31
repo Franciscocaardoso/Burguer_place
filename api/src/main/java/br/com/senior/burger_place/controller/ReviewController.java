32ee577dc359b08c807753fb4a460bb2fc2aeaa8
Franciscocaardoso <franciscoalyson.c@gmail.com>
2023-12-31 18:35:17 -0300

    Feito pequena refatoração

--- api/src/main/java/br/com/senior/burger_place/controller/ReviewController.java
+++ api/src/main/java/br/com/senior/burger_place/controller/ReviewController.java
@@ -1,8 +1,8 @@
 package br.com.senior.burger_place.controller;
 
 import br.com.senior.burger_place.domain.review.Review;
-import br.com.senior.burger_place.domain.review.dto.ReviewRegisterDTO;
 import br.com.senior.burger_place.domain.review.ReviewService;
+import br.com.senior.burger_place.domain.review.dto.ReviewRegisterDTO;
 import br.com.senior.burger_place.domain.review.dto.ReviewUpdateDTO;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -24,7 +24,7 @@ public class ReviewController {
 
     @PostMapping("/{occupationId}")
     @Transactional
-    public ResponseEntity<Object> registerReview(
+    public ResponseEntity<Object> register(
             @PathVariable
             Long occupationId,
             @RequestBody @Valid
@@ -37,33 +37,33 @@ public class ReviewController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<Review>> listAllReview(@PageableDefault(size = 10, sort = {"grade"})Pageable pageable){
+    public ResponseEntity<Page<Review>> listAllReview(@PageableDefault(size = 10, sort = {"grade"}) Pageable pageable) {
         Page<Review> reviews = service.listAllReview(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(reviews);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity listReviewById(@PathVariable Long id){
+    public ResponseEntity listReviewById(@PathVariable Long id) {
         Review review = service.listReviewById(id);
         return ResponseEntity.status(HttpStatus.OK).body(review);
     }
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity updateReview (
+    public ResponseEntity updateReview(
             @PathVariable
             Long id,
             @RequestBody
             @Valid
             ReviewUpdateDTO dto
-    ){
+    ) {
         ReviewRegisterDTO newReview = service.updateReview(id, dto);
         return ResponseEntity.status(HttpStatus.OK).body(newReview);
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public ResponseEntity<Object> deleteReview(@PathVariable Long id){
+    public ResponseEntity<Object> deleteReview(@PathVariable Long id) {
         service.deleteReview(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
