b9ef984a27e8b261e989501c7c65fa4bd7163e66
IgorIA <igorsemphoski@gmail.com>
2023-12-31 15:47:46 -0300

    Implement board list filters

--- web/src/app/components/board-list/board-list.component.ts
+++ web/src/app/components/board-list/board-list.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { Board } from './board';
+import {
+  Board,
+  BoardLocation,
+  BoardLocationType,
+} from '../../services/board.service';
+import { SvgImageComponent } from '../svg-image/svg-image.component';
 
 @Component({
   selector: 'app-board-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SvgImageComponent],
   templateUrl: './board-list.component.html',
   styleUrl: './board-list.component.css',
 })
@@ -19,5 +24,9 @@ export class BoardListComponent {
 
   public get boards() {
     return this._boards;
+  }
+
+  getBoardLocationText(type: BoardLocationType) {
+    return BoardLocation[type];
   }
 }
